686f4068e4dc5dbcc37333addad3814dc557a5c6
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-11-14 17:03:25 +0600

    handled local storage for ssr

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,6 @@
 const Config = {
-    baseApi: "http://localhost:4000",
+    // baseApi: "http://localhost:4000",
+    baseApi: "https://hotel-booking-system-server-lemon.vercel.app",
 };
 
 export default Config
--- src/app/Providers/AuthProvider.js
+++ src/app/Providers/AuthProvider.js
@@ -7,14 +7,31 @@ export const authContext = createContext(null);
 const AuthProvider = ({ children }) => {
 
     const [authInfo, setAuthInfo] = useState(null);
-    const [isAuthenticated, setIsAuthenticated] = useState(authInfo ? true : false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     useEffect(() => {
-        setAuthInfo(JSON.parse(localStorage.getItem('authInfo')))
-    }, [])
+        // Initialize authInfo from localStorage if available
+        const storedAuthInfo = localStorage.getItem('authInfo');
+        if (storedAuthInfo) {
+            const parsedAuthInfo = JSON.parse(storedAuthInfo);
+            setAuthInfo(parsedAuthInfo);
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        // Update localStorage and isAuthenticated when authInfo changes
+        if (authInfo) {
+            localStorage.setItem('authInfo', JSON.stringify(authInfo));
+            setIsAuthenticated(true);
+        } else {
+            localStorage.removeItem('authInfo');
+            setIsAuthenticated(false);
+        }
+    }, [authInfo]);
 
     return (
-        <authContext.Provider value={{ authInfo, isAuthenticated, setIsAuthenticated }}>
+        <authContext.Provider value={{ authInfo, isAuthenticated, setAuthInfo, setIsAuthenticated }}>
             {children}
         </authContext.Provider>
     );
