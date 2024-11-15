126892d726424001bf6a3f6418395b5c47cede13
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-11-15 20:49:30 +0600

    base api changed

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,7 @@
 const Config = {
-    baseApi: "http://localhost:4000",
+    // baseApi: "http://localhost:4000",
     // baseApi: "https://hotel-booking-system-server-lemon.vercel.app",
+    baseApi: "https://hotel-booking-system-server-sadi73-sadi73s-projects.vercel.app",
 };
 
 export default Config
